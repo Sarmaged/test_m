ae4efe1d60f900f2552efe7808310f95992cc5db
Sarmaged <sarmaged@gmail.com>
2022-03-26 21:10:36 +0300

    Add legacy / plugin-html and config HTML

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
+const path = require('path')
 import { defineConfig } from 'vite'
+import { minifyHtml, injectHtml } from 'vite-plugin-html'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    minifyHtml(),
+    injectHtml({
+      injectData: {
+        title: 'ProjectName',
+        description: 'A single page application created using Vue.js'
+      }
+    }),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+      '~bootstrap': 'bootstrap'
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/scss/variables";`
+      }
+    }
+  }
 })
