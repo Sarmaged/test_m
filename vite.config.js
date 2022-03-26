e696059327aab0a76d1eec60acbcf8d4f6f5bab1
Sarmaged <sarmaged@gmail.com>
2022-03-26 21:56:17 +0300

    Yarn berry

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 const path = require('path')
 import { defineConfig } from 'vite'
-import { minifyHtml, injectHtml } from 'vite-plugin-html'
-import legacy from '@vitejs/plugin-legacy'
+// import { minifyHtml, injectHtml } from 'vite-plugin-html'
+// import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    minifyHtml(),
-    injectHtml({
-      injectData: {
-        title: 'ProjectName',
-        description: 'A single page application created using Vue.js'
-      }
-    }),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
+    // minifyHtml(),
+    // injectHtml({
+    //   injectData: {
+    //     title: 'ProjectName',
+    //     description: 'A single page application created using Vue.js'
+    //   }
+    // }),
+    // legacy({
+    //   targets: ['ie >= 11'],
+    //   additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    // })
   ],
   resolve: {
     alias: {
